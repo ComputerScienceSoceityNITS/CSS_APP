e3cb0b47546235bf6e3a758f7b1c7af8079559c8
Raj <rajdristant007@gmail.com>
2023-01-13 01:19:33 +0530

    bitscribe pdf launch

---
+++ lib/screens/bitscribe_reader_screen/bitscribe_reader_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BitscribeReaderScreen extends StatefulWidget {
+  const BitscribeReaderScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BitscribeReaderScreen> createState() => _BitscribeReaderScreenState();
+}
+
+class _BitscribeReaderScreenState extends State<BitscribeReaderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(),
+      ),
+    );
+  }
+}
